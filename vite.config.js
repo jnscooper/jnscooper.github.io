87f119a0a0d6c622eb5b04e203c7babab8f9afe1
Jonas <jnscooper@qq.com>
2026-06-23 10:18:45 +0800

    start with vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { dirname, resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'index.html'),
+        nested: resolve(import.meta.dirname, 'nested/index.html'),
+      },
+    },
+  },
+})
